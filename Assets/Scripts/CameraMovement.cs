9668f56ead17c524a3494ed943a188d13df1f46d
Bianca M Davey <60249994+BiancaDavey@users.noreply.github.com>
2025-10-29 15:35:11 +0000

    Added camera boundaries based on scene boundaries

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,13 +5,19 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform target;
     private float smooth = 1;
+    public Vector2 maxXY;
+    public Vector2 minXY;
 
     void LateUpdate()
     {
-        //  Move camera position to target position.
         if(transform.position != target.position)
         {
+            //  Set target position.
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            //  Set xy coordinates for camera boundary for scene.
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minXY.x, maxXY.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minXY.y, maxXY.y);
+            //  Move camera position to target position.
             transform.position = Vector3.Lerp(transform.position, targetPosition, smooth);
         }
     }
